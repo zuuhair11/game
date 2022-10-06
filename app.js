b8259d99ffb5b4cbd6ad9d7d9c99a3df9dee0cc5
Zouhair <zouhairsahtout66@gmail.com>
2022-10-06 12:21:49 -0400

    returning the template instead innerHTM

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// The Hero Object
-const hero = {
-    elementId: "hero", 
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceCount: 3
-}
-
-// The Monster Object
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceCount: 1
-}
-
 // Will generate a random numbers of each object based on thier diceCount
 function getDiceRollArray(diceCount) {
     return new Array(diceCount).fill(0).map(function() {
@@ -23,40 +5,56 @@ function getDiceRollArray(diceCount) {
     })
 }
 
-// This Func will inovoked the getDiceRollArray(), 
-// So that can put each number in a div and return it
-
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function(number) {
-        return `<div class="dice">${number}</div>`;
-    }).join('');
+const characterData = {
+    hero: {
+        elementId: "hero", 
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: 60,
+        diceCount: 3
+    },
+    monster: {
+        elementId: "monster",
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: 10,
+        diceCount: 1
+    }
 }
 
-
+// This Func will inovoked the getDiceRollArray(), 
+// So that can put each number in a div and return it
 function Character(data) {
     // I made copy of my object 'data' to 'this', which is my current object
     Object.assign(this, data);
 
+    this.getDiceHtml = function(diceCount) {
+        return getDiceRollArray(diceCount).map(function(number) {
+            return `<div class="dice">${number}</div>`;
+        }).join('')
+    }
     this.gitCharacterHtml = function() {
         const {elementId, name, avatar, health, diceCount} = this;
 
         // Invoking the getDiceHtml() passing to it the diceCount of the target obj
-        const diceHtml = getDiceHtml(diceCount);
-
-        document.getElementById(elementId).innerHTML = 
-        `<div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}"/>
-            <p class="health">health: <b> ${health}</b></p>
-            <div class="dice-container">
-                ${diceHtml}
-            </div>
-        </div>`;
+        const diceHtml = this.getDiceHtml(diceCount);
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}"/>
+                <p class="health">health: <b> ${health}</b></p>
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`;
     }
 }
 
-const wizard = new Character(hero);
-wizard.gitCharacterHtml();
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 
-const orc = new Character(monster);
-orc.gitCharacterHtml();
+function render() {
+    document.getElementById(wizard.elementId).innerHTML = wizard.gitCharacterHtml();
+    document.getElementById(orc.elementId).innerHTML = orc.gitCharacterHtml();
+}
+render();
